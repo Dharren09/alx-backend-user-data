b6705c5a59fdb38da4a6ea76e666b89d73b7bd2d
Dharren09 <dharrydharrenzerah@gmail.com>
2023-05-25 17:49:18 +0300

    Encryting passwords

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -11,3 +11,8 @@ def hash_password(password: str) -> bytes:
     hashed_password = bcrypt.hashpw(password.encode("utf-8"), salt)
 
     return hashed_password
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """function compares hashed password to the provided password
+    for similarity"""
+    return bcrypt.checkpw(password.encode("utf-8"), hashed_password)
