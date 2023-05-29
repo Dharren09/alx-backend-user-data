f6376770501f61963ccdd67405e50fe622501a79
Dharren09 <dharrydharrenzerah@gmail.com>
2023-05-29 19:22:23 +0300

    testsing new error handler

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -1,25 +1,18 @@
 #!/usr/bin/env python3
-""" Module of Index views
-"""
+"""error handler"""
 from flask import jsonify, abort
 from api.v1.views import app_views
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status() -> str:
-    """ GET /api/v1/status
-    Return:
-      - the status of the API
-    """
+    """GET /api/v1/status"""
     return jsonify({"status": "OK"})
 
 
 @app_views.route('/stats/', strict_slashes=False)
 def stats() -> str:
-    """ GET /api/v1/stats
-    Return:
-      - the number of each objects
-    """
+    """the number of each objects"""
     from models.user import User
     stats = {}
     stats['users'] = User.count()
@@ -28,17 +21,11 @@ def stats() -> str:
 
 @app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
 def unauthorized() -> str:
-    """ GET /api/v1/unauthorized
-    Return:
-      - abort(401)
-    """
+    """GET /api/v1/unauthorized"""
     abort(401)
 
 
 @app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
 def forbidden() -> str:
-    """ GET /api/v1/forbidden
-    Return:
-      - abort(403)
-    """
+    """GET /api/v1/forbidden"""
     abort(403)
