cd3f88db88fda6e80b02e4f9250911f67df42f16
Dharren09 <dharrydharrenzerah@gmail.com>
2023-05-31 18:02:41 +0300

    Session Auth

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""session authentication module"""
+
+from api.v1.auth.auth import Auth
+from uuid import uuid4
+from models.user import User
+
+
+class SessionAuth(Auth):
+    pass
